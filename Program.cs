aecf7abb879df7e4ebc6a927d27219220636eddd
pkid1729 <pkid1729@gmail.com>
2018-09-08 01:19:26 -0700

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_concat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name ");
+            Console.Write("Enter your first name ");
+            string myfirstname;
+            myfirstname = Console.ReadLine();
+            Console.Write("Enter your last name ");
+            string mylastname;
+            mylastname = Console.ReadLine();
+            Console.WriteLine("Hello  " + myfirstname + " " + mylastname);
+            Console.ReadLine();
+
+        }
+    }
+}
